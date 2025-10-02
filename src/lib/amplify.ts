775b5191e1e517821bdd1e64e776f2beccd7dfdb
hello112334 <hello112334@gmail.com>
2025-10-02 21:44:35 +0900

    reinit

--- src/lib/amplify.ts
+++
@@ -1,9 +0,0 @@
-import { Amplify } from 'aws-amplify'
-import outputs from '../../amplify_outputs.json'
-
-// Configure Amplify for Gen2
-Amplify.configure(outputs, {
-  ssr: true, // Enable server-side rendering support
-})
-
-export default Amplify
--- src/lib/data-client.ts
+++
@@ -1,7 +0,0 @@
-import { generateClient } from 'aws-amplify/data'
-import type { Schema } from '../../amplify/data/resource'
-
-// Generate the data client with full type safety
-export const client = generateClient<Schema>()
-
-export type { Schema }
